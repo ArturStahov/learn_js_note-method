5dea93d1fc21f9081be6ade5a5842929e7d7dc23
ArturStahov <vtemy.at.ua@gmail.com>
2020-08-27 23:00:04 +0300

    fix 1.0

--- observer-methods/observer.js
+++ observer-methods/observer.js
@@ -1,12 +1,21 @@
-const observerApi = new IntersectionObserver((entries, options) => {
+const onEntries = (entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       console.log("object is intersecting");
 
       entry.target.style.backgroundColor = generateRandomColor();
+      //   observer.disconnect(); if need stop fn  observer
+      //   observer.unobserve(entry); if need stopped observe for some element
     }
   });
-});
+};
+
+const options = {
+  rootMargin: "50px", //margin begin observe
+  threshold: [0, 0.25, 0.5, 1] //areas elem views in browser viewport (default=1)
+};
+
+const observerApi = new IntersectionObserver(onEntries, options);
 
 const observerElemRef = document.querySelector(".observer-elem");
 
@@ -14,7 +23,7 @@ observerApi.observe(observerElemRef);
 
 const generateRandomColor = () => {
   const getRandomIntInclusive = (min, max) => {
-    //функция рандомной генерации числа в диапазоне min max
+    //fn random generate int min max
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
